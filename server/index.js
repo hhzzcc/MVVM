8bebc94a62dd2615daff7170b0eb488f000a8503
lingmao <lingmao@gaoding.com>
2020-11-04 18:35:00 +0800

    feat: 调整端口

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 
-const express = require('express')
-const path = require('path')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
 //提供静态文件服务
-app.use(express.static(path.resolve(__dirname , '../')))
+app.use(express.static(path.resolve(__dirname , '../')));
 
 
-let port = 1234
+let port = 5555;
 app.listen(port , function () {
-    console.log(`连接成功,请访问http://localhost:${port}`)
+    console.log(`连接成功,请访问http://localhost:${port}`);
 });
